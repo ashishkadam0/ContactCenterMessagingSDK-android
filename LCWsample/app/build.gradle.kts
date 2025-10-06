91cd99d42225197a0b77b6bdb9a3810725b100c1
Ashish <ashishkadam@microsoft.com>
2025-10-06 16:18:39 +0530

    min sdk 23

--- LCWsample/app/build.gradle.kts
+++ LCWsample/app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
 
     defaultConfig {
         applicationId = "com.ms.lcwsamplapp"
-        minSdk = 26
+        minSdk = 23
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
